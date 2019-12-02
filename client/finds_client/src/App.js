0b6a1383b7924262d711fe0f385ec1e196192b9c
kiksun <kimera4173@gmail.com>
2019-12-02 20:03:33 +0900

    [add]検索フォームからSearchResultPageへの遷移機能の実装

--- client/finds_client/src/App.js
+++ client/finds_client/src/App.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { ConnectedRouter } from "connected-react-router";
 import { Switch, Route } from "react-router-dom";
 import history from "./helpers/history";
-
+import Result from "./components/SearchResultpage";
 import Login from "./components/login";
 import Home from "./components/home";
 import Error from "./components/Error";
 import DocumentDetailPage from "./components/DocumentDetailPage";
 import MyPage from "./components/myPage";
-import Result from "./components/SearchResultpage";
 
 function App() {
   return (
@@ -16,8 +15,8 @@ function App() {
       <Switch>
         <Route exact path="/login" component={Login} />
         <Route exact path="/home" component={Home} />
-        <Route exact path="/result" component={Result}/>
         <Route path="/detail" component={DocumentDetailPage} />
+        <Route exact path="/result" component={Result}/>
         <Route exact path="/myPage" component={MyPage} />
         <Route path="/" component={Login} />
       </Switch>
