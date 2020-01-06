69ac84d00223989115f327cee6bdf97de2137717
b1018043 <grey.fox.0417@gmail.com>
2020-01-06 15:21:46 +0900

    add:購入時に通信を行うようにした

--- client/finds_client/src/reducers/SelectDoc.js
+++ client/finds_client/src/reducers/SelectDoc.js
@@ -7,9 +7,9 @@ const initState={
 export default function selectDocReducer(state=initState,action){
     switch(action.type){
         case MOVE_DOC_DETAIL:
-            const {name,creator,creatorLevel,creatorImg,likes,reviews,dl,description,comments,autherId,thumbnail}=action.payload;
-            const tmp = { name, creator, creatorLevel, creatorImg, likes, reviews, dl, description, comments, autherId, thumbnail}
-            sessionStorage.setItem("finds-target",tmp);
+            const {name,creator,creatorLevel,creatorImg,likes,reviews,dl,description,comments,autherId,thumbnail,price}=action.payload;
+            const tmp = { name, creator, creatorLevel, creatorImg, likes, reviews, dl, description, comments, autherId, thumbnail,price};
+            sessionStorage.setItem("finds-target", JSON.stringify(tmp));
             return {target:action.payload};
         default:
             return state;
--- client/finds_client/src/sagas/index.js
+++ client/finds_client/src/sagas/index.js
@@ -3,9 +3,10 @@ import UserDocSaga from "./UserDoc";
 import SearchSaga from "./Search";
 import UserInfoSaga from "./UserInfo";
 import UserLoginSaga from "./UserLogin";
+import BuyDocSaga from "./buyDoc";
 
 export default function* rootSaga(){
     yield all([
-        ...UserDocSaga,...UserInfoSaga,...SearchSaga,...UserLoginSaga
+        ...UserDocSaga,...UserInfoSaga,...SearchSaga,...UserLoginSaga,...BuyDocSaga
     ]);
 }
