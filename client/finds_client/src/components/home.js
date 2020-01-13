f7f0eb52aa04f9b970e2d24a27248bbaaa0562bc
MasatakaKudou <b1017027@fun.ac.jp>
2020-01-14 01:02:43 +0900

    [add]アップロード画面に遷移できるようにして、ボタン固定した

--- client/finds_client/src/components/home.js
+++ client/finds_client/src/components/home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { push } from "connected-react-router";
+import { useDispatch, useSelector } from "react-redux";
 import TopAppBar from './TopAppBar';
 import MyDocumentList from './MyDocumentsList';
 import Fab from '@material-ui/core/Fab';
@@ -6,13 +8,19 @@ import AddIcon from '@material-ui/icons/Add';
 import '../styles/home.css';
 
 const Home = props => {
+    const dispatch = useDispatch();
+    const moveToUploadPage = () => {
+        return (dispatch(push("/uploadPage")));
+    }
     return (
-        <div className="positionStandard">
+        <div>
             <TopAppBar />
             <MyDocumentList />
-            <Fab className="positionAbsolute" color="primary" aria-label="add">
-                <AddIcon />
-            </Fab>
+            <div className="positionFixed">
+                <Fab color="primary" aria-label="add" className="positionFixed" onClick={moveToUploadPage}>
+                    <AddIcon />
+                </Fab>
+            </div>
         </div>
     )
 }
