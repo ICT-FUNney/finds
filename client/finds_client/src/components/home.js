ffd22c0a29281287ffd7599651322e9abcc59841
MasatakaKudou <b1017027@fun.ac.jp>
2020-01-13 22:59:59 +0900

    [add]home.cssの追加

--- client/finds_client/src/components/home.js
+++ client/finds_client/src/components/home.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import TopAppBar from './TopAppBar';
 import MyDocumentList from './MyDocumentsList';
-
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import '../styles/home.css';
 
 const Home = props => {
     return (
-        <div>
+        <div className="positionStandard">
             <TopAppBar />
-            <MyDocumentList/>
+            <MyDocumentList />
+            <Fab className="positionAbsolute" color="primary" aria-label="add">
+                <AddIcon />
+            </Fab>
         </div>
     )
 }
